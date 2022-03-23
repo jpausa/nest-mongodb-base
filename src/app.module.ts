f6c2f6dde6f73186deff449f26618be6bc403eca
Jorge <glph8911@gmail.com>
2022-03-23 10:25:33 -0500

    dockerizing the app and adding controller and provider to TestModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TestModule } from './test-module/test-module';
 import { AppConfigModule } from './app-config-module/app-config.module';
-import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [TestModule, AppConfigModule],
