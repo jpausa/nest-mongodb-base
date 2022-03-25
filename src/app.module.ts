b7da2c6a8f7b9ca8b9faf374af9455c72cae75dc
Jorge <glph8911@gmail.com>
2022-03-25 11:59:06 -0500

    removing unnecessary module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TestModule } from './test-module/test-module';
 import { AppConfigModule } from './app-config-module/app-config.module';
 import { MongoTestModule } from './mongo-test/mongo-test.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [TestModule, AppConfigModule, MongoTestModule, DatabaseModule],
+  imports: [AppConfigModule, MongoTestModule, DatabaseModule],
   controllers: [],
   providers: [],
 })
